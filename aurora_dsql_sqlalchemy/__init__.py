f3805c9b711838c861cd5a7d987d79267dbf8754
Victor Tsang <vitsangp@amazon.com>
2025-05-21 17:46:07 -0700

    remove psycopg reference

--- aurora_dsql_sqlalchemy/__init__.py
+++ aurora_dsql_sqlalchemy/__init__.py
@@ -3,5 +3,3 @@ from sqlalchemy.dialects import registry
 __version__ = "0.0.0.1"
 
 registry.register("aurora_dsql.psycopg2", "aurora_dsql_sqlalchemy.psycopg2", "AuroraDSQLDialect_psycopg2")
-registry.register("aurora_dsql.psycopg", "aurora_dsql_sqlalchemy.psycopg", "AuroraDSQLDialect_psycopg")
-
