222dbab31619110a08bcd999d0de49dd0f5df354
Daniel Frankcom <frankcom@amazon.com>
2026-01-20 11:45:37 -0800

    Add missing copyright headers

--- aurora_dsql_sqlalchemy/psycopg2.py
+++ aurora_dsql_sqlalchemy/psycopg2.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from sqlalchemy.dialects.postgresql.psycopg2 import PGDialect_psycopg2
 
 from .base import AuroraDSQLDialect
